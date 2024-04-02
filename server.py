4e1e778b2d5267bd8f5a7cad1a3e4d56981e68f9
mikhailemelin <emelinmd@yandex.ru>
2024-04-02 15:23:26 +0300

    Доработки по второму дедлайну

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from models import Section, Topic
+from models import Section
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
@@ -12,7 +11,11 @@ db = SQLAlchemy(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html")
+    params = {
+        'title': 'Супер'
+    }
+    return render_template("index.html", **params)
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -20,21 +23,25 @@ def upload_file():
         return redirect(url_for('index'))
     return render_template('upload.html')
 
+
 @app.route('/ask_question', methods=['GET', 'POST'])
 def ask_question():
     if request.method == 'POST':
         return redirect(url_for('index'))
     return render_template('ask_question.html')
 
+
 @app.route('/sections')
 def sections():
     all_sections = Section.query.all()
     return render_template('sections.html', sections=all_sections)
 
+
 @app.route('/sections/<int:section_id>')
 def topics(section_id):
     section = Section.query.get_or_404(section_id)
     return render_template('topics.html', section=section)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
